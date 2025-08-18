935e1fd31ceedcfe292a104e257af9e4a4a94544
BxNoel <noelnegron03@gmail.com>
2025-08-18 19:40:23 -0400

    adding inital changes

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import UniversityNewsPage from './Pages/UniversityNewsPage';
+import { Routes, Route } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <div>
+        <Routes>
+          <Route path="/" element={<UniversityNewsPage />} />
+        </Routes>
+    </div>
+  );
+}
+
+export default App;
