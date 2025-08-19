a866395abeb04a51633465a5e141f9953814d3fa
BxNoel <noelnegron03@gmail.com>
2025-08-18 21:23:23 -0400

    adding sample objects

---
+++ src/Components/SampleComponent.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    background-color: #f0f0f0;
+    border-radius: 8px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    font-family: Arial, sans-serif;
+    `;
+    
+const SampleComponent = () => {
+    return (
+        <Container>
+            components
+        </Container>
+        );
+    };
+
+export default SampleComponent;
--- src/Pages/UniversityNewsPage.js
+++ src/Pages/UniversityNewsPage.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import styled from 'styled-components';
+import SampleComponent from '../Components/SampleComponent';
+
+const StyledText = styled.div` 
+    font-size: 20px;
+    color: #333;
+    padding: 20px;
+    background-color: #f9f9f9;
+    border-radius: 8px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+`;
 
 const UniversityNewsPage = () => {
     return (
-        <div>
-           Start Working here
-        </div>
+        <>
+            <StyledText>
+            Start Working here
+            </StyledText>
+         <SampleComponent/>
+         </>
         );
     };
 
