6d89851612d45d1086aa7b90080c9dc251e48e49
BxNoel <noelnegron03@gmail.com>
2025-08-24 23:03:44 -0400

    adding firework and reworking credits responsivness

--- src/Components/CreditsComponent.js
+++ src/Components/CreditsComponent.js
@@ -1,114 +1,190 @@
 import React from "react";
 import styled from "styled-components";
-
-const Page = styled.main`
-  min-height: 100vh;
-  background-color: #3EB8E7;
-  padding-top: 96px;
-  padding-bottom: 96px;
-`;
+import Firework from "./Firework"; // Import the BurstIcon component
+import { credits } from "../data/credits";
 
 const Container = styled.div`
-  box-sizing: border-box;
+  position: relative;
+  margin-top: 4rem;
+  z-index: 5;
+  color: black;
+  background-color: #fff;
+  padding: 2rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border-radius: 10px;
+  font-family: "Righteous";
+
+  @media only screen and (max-width: 768px) {
+    padding: 1rem;
+  }
+`;
+
+const BurstWrapper = styled.div`
+  position: absolute;
+  z-index: 1;
+  &.top-left {
+    top: -50px;
+    left: -50px;
+
+    @media only screen and (max-width: 768px) {
+      top: -30px;
+      left: -30px;
+    }
+  }
+
+  &.top-right {
+    top: 30px;
+    right: 40px;
+
+    @media only screen and (max-width: 768px) {
+      top: 10px;
+      right: 10px;
+    }
+  }
+
+  &.bottom-left {
+    bottom: 30px;
+    left: 40px;
+
+    @media only screen and (max-width: 768px) {
+      bottom: 10px;
+      left: 20px;
+    }
+  }
+
+  &.bottom-right {
+    bottom: -50px;
+    right: -50px;
+
+    @media only screen and (max-width: 768px) {
+      bottom: -30px;
+      right: -30px;
+    }
+  }
+`;
+const HeaderDiv = styled.div`
+  z-index: 2; 
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: black;
+  width: 80%;
+  margin-bottom: 35px;
+`;
+
+const Header = styled.h1`
+  font-size: 37px;
+  font-family: "Righteous";
+  font-weight: 400;
+  line-height: 2.5rem;
+  text-align: center;
+  color: black;
+
+  @media only screen and (max-width: 768px) {
+    font-size: 24px;
+    line-height: 2rem;
+  }
+`;
+
+const SectAndNames = styled.div`
+  z-index: 2; 
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 1rem;
   width: 100%;
-  padding-left: 180px;
-  padding-right: 230px;
+  margin-top: 1rem;
+
+  @media only screen and (max-width: 1024px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media only screen and (max-width: 768px) {
+    grid-template-columns: 1fr;
+  }
 `;
 
+const Section = styled.div`
+  font-size: 1.8rem;
+  font-weight: 400;
+  margin-bottom: 2rem;
+  font-family: "Righteous";
+  color: black;
 
+  @media only screen and (max-width: 1216px) {
+    font-size: 1.6rem;
+  }
 
-const LetterCard = styled.div`
-  box-sizing: border-box;
-  background: #f7f7f7;
-  border: 8.729px solid #000;
-  padding: 25px 85px 100px;
-  color: #2C2C2C;
-  font-family: 'Righteous';
-  line-height: 32px;
+  @media only screen and (max-width: 1024px) {
+    font-size: 1.5rem;
+  }
 
-  h2 {
-    font-size: 34.6px;
-    font-weight: 400;
+  @media only screen and (max-width: 768px) {
+    font-size: 20px;
     text-align: center;
-    margin-bottom: 25px;
-  }
-
-  .credits-grid {
-    display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(180px, 1fr));
-    gap: 50px;
-  }
-  
-  .section h3 {
-    font-size: 28.65px;
-    font-weight: 400;
-    margin-bottom: 12px;
-  }
-
-  .section p {
-    font-size: 24.88px;
-    font-weight: 400;
-    margin: 12px 0;
   }
 `;
 
+const Name = styled.div`
+  font-family: "Righteous";
+  font-size: 18px;
+  font-weight: 400;
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+  color: black;
+
+  @media only screen and (max-width: 1216px) {
+    font-size: 1rem;
+  }
+
+  @media only screen and (max-width: 1024px) {
+    font-size: 0.9rem;
+  }
+
+  @media only screen and (max-width: 768px) {
+    font-size: 14px;
+  }
+`;
 
 const CreditsComponent = () => {
-    return (
-        <Page>
-            <Container>
-                <LetterCard>
-                    <h2>Staff Who Made This Issue Possible</h2>
-                    <div className="credits-grid">
-                        <div className="section">
-                            <h3>Section</h3>
-                            <p>First Last, Title</p>
-                            <p>First Last, Title</p>
-                            <p>First Last, Title</p>
-                            <p>First Last, Title</p>
-                        </div>
-                        <div className="section">
-                            <h3>Section</h3>
-                            <p>First Last, Title</p>
-                            <p>First Last, Title</p>
-                            <p>First Last, Title</p>
-                            <p>First Last, Title</p>
-                        </div>
-                        <div className="section">
-                            <h3>Section</h3>
-                            <p>First Last, Title</p>
-                            <p>First Last, Title</p>
-                            <p>First Last, Title</p>
-                            <p>First Last, Title</p>
-                        </div>
-                    </div>
-                    <div className="credits-grid">
-                        <div className="section">
-                            <h3>Section</h3>
-                            <p>First Last, Title</p>
-                            <p>First Last, Title</p>
-                            <p>First Last, Title</p>
-                            <p>First Last, Title</p>
-                        </div>
-                        <div className="section">
-                            <h3>Section</h3>
-                            <p>First Last, Title</p>
-                            <p>First Last, Title</p>
-                            <p>First Last, Title</p>
-                            <p>First Last, Title</p>
-                        </div>
-                        <div className="section">
-                            <h3>Section</h3>
-                            <p>First Last, Title</p>
-                            <p>First Last, Title</p>
-                            <p>First Last, Title</p>
-                            <p>First Last, Title</p>
-                        </div>
-                    </div>
-                </LetterCard>
-            </Container>
-        </Page>
-    );
+  return (
+    <Container id="credits">
+      
+      {/* Top-left Burst */}
+      <BurstWrapper className="top-left">
+        <Firework fillColor="#F0FFA3" strokeColor="#111" size="large-firework"/>
+      </BurstWrapper>
+      <BurstWrapper className="top-right">
+        <Firework fillColor="#F0FFA3" strokeColor="#111" size="small-firework"/>
+      </BurstWrapper>
+      <BurstWrapper className="bottom-left">
+        <Firework fillColor="#F0FFA3" strokeColor="#111" size="small-firework" />
+      </BurstWrapper>
+      <BurstWrapper className="bottom-right">
+        <Firework fillColor="#F0FFA3" strokeColor="#111" size="large-firework" />
+      </BurstWrapper>
+      <HeaderDiv>
+        <Header>Staff Who Made This Issue Possible</Header>
+      </HeaderDiv>
+      <SectAndNames>
+        {Object.entries(credits).map(([section, key]) => (
+          <Section key={section}>
+            {section}
+            <br />
+            {key.map((person, index) => (
+              <Name key={index}>
+                {person.staff_name}, {person.title}
+              </Name>
+            ))}
+            <br />
+          </Section>
+        ))}
+      </SectAndNames>
+    </Container>
+  );
 };
 
 export default CreditsComponent;
--- src/Pages/UniversityNewsPage.js
+++ src/Pages/UniversityNewsPage.js
@@ -1,16 +1,50 @@
 import React from 'react';
 import styled from 'styled-components';
 import IntroPage from './IntroPage';
+import NavBar from '../Components/NavBar';
+import SectionTitle from '../Components/SectionTitle';
+import ArticlePage from './ArticlePage';
+
+import universityArticles from "../data/universityArticles";
+import CreditsComponent from '../Components/CreditsComponent';
 
 const PageWrapper = styled.div`
+  min-height: 160vh;
+  background-color: #3EB8E7;
+  margin-top: 2.5rem;
 
+@media (max-width: 768px) {
+  background-color: #0D9DD4;          
+}
 `;
+
+const CreditsWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 2rem;
+
+  @media (max-width: 768px) {
+    margin-top: 10rem;
+  }
+`;
+
+
 
 const UniversityNewsPage = () => {
     return (
-        <PageWrapper>
-            <IntroPage/>
-        </PageWrapper>            
+        <div>
+          <IntroPage></IntroPage>
+            <SectionTitle title="UNIVERSITY" />
+            <PageWrapper>
+                <NavBar></NavBar>
+                <ArticlePage articles={universityArticles} />
+            </PageWrapper>
+            <CreditsWrapper>
+              <CreditsComponent />
+            </CreditsWrapper>  
+        </div>
+          
         );
     };
 
